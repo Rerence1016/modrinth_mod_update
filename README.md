b6e26ca20834bed47b8b64ecba8c81400af7a822
Rerence1016 <schp4544@gmail.com>
2025-07-30 20:01:43 +0800

    Add command-line arguments, and update README

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
-## modrinth_mod_update
+# modrinth_mod_update
 
 Python script for updating your Minecraft mods from Modrinth. No external modules required.
 
-Features:
+### Features:
 
 - Stores mod info as .json file (and detects if one already exists in the same folder as script)
 - Checks for newer version from Modrinth and compares version to installed mods
 - Downloads newer versions if detected
 - SHA1 checksum for file integrity
+- Supports command line arguments (*wow!*)
+
+### Usage:
+
+There are no external Python modules to install, only uses built-in libraries. Just install Python 3
+on your computer, and run it through a terminal or command prompt. Has only been tested on Linux,
+Python 3.13.5. I haven't tested on Windows or macOS, but should theoretically be platform independent.
+
+You can use `mod_update.py --help` for the help message.
+
+## Detecting and creating a `.json` file
+
+This detects whether an existing `.json` file exists. If not, then it'll ask for what folder your mods
+are in, and creates a `.json` file to cache for faster and easier use, and which you can specify the
+name of in the command.
+
+`python mod_update.py --detect mods.json`
+
+You can even use paths like `/home/username/Downloads/mods.json` or even `~/Downloads/mods.json` to
+specify your `.json` file! Really high tech, I know.
+
+## Checking for updates
+
+Assuming a `.json` file for your mods exists, you can specify it with the command below. It checks for
+the mods present in the file, then checks Modrinth for a newer version. If newer ones are detected, it
+downloads the file, verifies its integrity with a checksum, and replaces the old mod.
+
+`python mod_update.py --check mods.json`
+
+## Reporting bugs
+
+Make a pull request at this repository's [GitHub Issues](https://github.com/Rerence1016/modrinth_mod_update/issues).
+Provide a detailed description of:
+
+- What you were trying to do
+- What doesn't work
+- Python version
+- OS and version
+- A screenshot or `.txt` of the error or bug
--- mod_update.py
+++ mod_update.py
@@ -1,15 +1,15 @@
-from packaging.version import parse
+from packaging.version import parse as versionParse
 import requests
 import zipfile
 import hashlib
 import shutil
 import json
+import argparse
 import os
 import re
 
 # Hard coded values (customizable according to taste)
 TEMP_DIR = "mod_update-temp"
-JSON_FILE_NAME = "mods"
 TARGET_MINECRAFT_VERSION = '1.21.4'
 LOADER = 'fabric'
 
@@ -25,14 +25,14 @@ header = {'User-Agent': "modrinth_mod_update.py (Rerence1016 on GitHub)"}
 # HTTP code handling
 def HTTP_code_handling(HTTP_request: int):
 	if (HTTP_request == 410):
-		print(f"{HTTP_request}: WARNING: MODRINTH API HAS BEEN DEPRECATED! PLEASE USE WHATEVER IS NEW!!")
+		print(f"{HTTP_request}: WARNING: MODRINTH API HAS BEEN DEPRECATED! PLEASE USE WHATEVER IS NEW!")
 	elif (HTTP_request == 404):
-		print(f"{HTTP_request}: ID OR SLUG IS INVALID: PROJECT NOT FOUND!!")
+		print(f"{HTTP_request}: ID OR SLUG IS INVALID: PROJECT NOT FOUND!")
 	elif (HTTP_request == 200):
 		# Success
 		pass
 	else:
-		print(f"{HTTP_request}: SOME ERROR HAS HAPPENED (IDK)")
+		print(f"{HTTP_request}: ERROR OCCURRED!")
 
 # Strip filenames to just versions with RegEx
 # RegEx is hard coded for only Minecraft version 1.21.4, sadly
@@ -47,65 +47,61 @@ def clean_version(string_to_clean: str):
 # A - Initial startup
 
 # Detect if .json already exists
-if f"{JSON_FILE_NAME}.json" not in os.listdir("."):
-	# No .json detected (has to be explicitly named according to `JSON_FILE_NAME`)
-	print(f"NO \"{JSON_FILE_NAME}.json\" DETECTED!")
-	print("Making new one...")
-	mods = {}
-	MODS_FOLDER_PATH = input("Please type what folder your mods are located in: \n")
-	ABSOLUTE_MOD_PATH = f"{os.path.abspath(MODS_FOLDER_PATH)}"
-	os.chdir(ABSOLUTE_MOD_PATH)
-	folder_mods = os.listdir(f"{ABSOLUTE_MOD_PATH}")
-	os.chdir(os.path.dirname(os.path.abspath(__file__)))
-	print(f"Mods found in \"{MODS_FOLDER_PATH}/\":\n")
+def detect(JSON_FILE_NAME):
+	if f"{JSON_FILE_NAME}" not in os.listdir("."):
 
-	# Obtain version and slug from existing files in a directory
-	for mod in folder_mods:
-		with zipfile.ZipFile(f"{ABSOLUTE_MOD_PATH}/{mod}", 'r') as modfile:
-			modfile.extract("fabric.mod.json", f"{TEMP_DIR}/")
-		os.rename(f"{TEMP_DIR}/fabric.mod.json", f"{TEMP_DIR}/{mod}.json")
-		mod_json = json.load(open(f"{TEMP_DIR}/{mod}.json"))
-		mod_name = mod_json["name"]
+		# No .json detected (has to be explicitly named according to `JSON_FILE_NAME`)
+		print(f"NO \"{JSON_FILE_NAME}\" DETECTED!")
+		print("Making new one...")
+		mods = {}
+		MODS_FOLDER_PATH = input("Please type what folder your mods are located in: \n")
+		ABSOLUTE_MOD_PATH = f"{os.path.abspath(MODS_FOLDER_PATH)}"
+		os.chdir(ABSOLUTE_MOD_PATH)
+		folder_mods = os.listdir(f"{ABSOLUTE_MOD_PATH}")
+		os.chdir(os.path.dirname(os.path.abspath(__file__)))
+		print(f"Mods found in \"{MODS_FOLDER_PATH}/\":\n")
 
-		# Parameters for searching mods from existing files, i.e., connect them to Modrinth
-		params = {'query': f"{mod_name}"}
-		PROJECT_GET = requests.get(f"{MODRINTH_URL}/search", headers=header, params=params)
-		HTTP_code_handling(PROJECT_GET.status_code)
-		results = json.loads(PROJECT_GET.text)
+		# Obtain version and slug from existing files in a directory
+		for mod in folder_mods:
+			with zipfile.ZipFile(f"{ABSOLUTE_MOD_PATH}/{mod}", 'r') as modfile:
+				modfile.extract("fabric.mod.json", f"{TEMP_DIR}/")
+			os.rename(f"{TEMP_DIR}/fabric.mod.json", f"{TEMP_DIR}/{mod}.json")
+			mod_json = json.load(open(f"{TEMP_DIR}/{mod}.json"))
+			mod_name = mod_json["name"]
+
+			# Parameters for searching mods from existing files, i.e., connect them to Modrinth
+			params = {'query': f"{mod_name}"}
+			PROJECT_GET = requests.get(f"{MODRINTH_URL}/search", headers=header, params=params)
+			HTTP_code_handling(PROJECT_GET.status_code)
+			results = json.loads(PROJECT_GET.text)
+			
+			# Setting JSON files
+			version = clean_version(mod)
+			slug = results["hits"][0]["slug"]
+			path = f"{ABSOLUTE_MOD_PATH}/{mod}"
+
+			print(f"Filename: {mod} \n\tName: {mod_name} \n\tSlug: {slug} \n\tVersion: {version} \n")
+			mods[f"{slug}"] = {"name": f"{mod_name}", "slug": f"{slug}", "version": f"{version}", "path": f"{path}"}
 		
-		# Setting JSON files
-		version = clean_version(mod)
-		slug = results["hits"][0]["slug"]
-		path = f"{ABSOLUTE_MOD_PATH}/{mod}"
+		mods["mods_folder_path"] = f"{ABSOLUTE_MOD_PATH}"
+		# Write to .json file
+		MOD_INFO_JSON = json.dumps(mods, indent=4)
+		MODS_JSON = open(f"{JSON_FILE_NAME}.json", 'w')
+		MODS_JSON = MODS_JSON.write(MOD_INFO_JSON)
 
-		print(f"Filename: {mod} \n\tName: {mod_name} \n\tSlug: {slug} \n\tVersion: {version} \n")
-		mods[f"{slug}"] = {"name": f"{mod_name}", "slug": f"{slug}", "version": f"{version}", "path": f"{path}"}
-	
-	mods["mods_folder_path"] = f"{ABSOLUTE_MOD_PATH}"
-	# Write to .json file
-	MOD_INFO_JSON = json.dumps(mods, indent=4)
-	MODS_JSON = open(f"{JSON_FILE_NAME}.json", 'w')
-	MODS_JSON = MODS_JSON.write(MOD_INFO_JSON)
+		# Open .json file
+		# Not putting all the code in a `with as` statement to make it less confusing
+		MODS_JSON = open(f"{JSON_FILE_NAME}.json")
+		MODS_JSON = MODS_JSON.read()
+		MODS_JSON = json.loads(MODS_JSON)
 
-	# Open .json file
-	# Not putting all the code in a `with as` statement to make it less confusing
-	MODS_JSON = open(f"{JSON_FILE_NAME}.json")
-	MODS_JSON = MODS_JSON.read()
-	MODS_JSON = json.loads(MODS_JSON)
-
-	print(f"Mod info has been written to {JSON_FILE_NAME}.json: Successful \n")
-	path = MODS_JSON["mods_folder_path"]
-	shutil.rmtree(f"{TEMP_DIR}")
-	print("Deleted temp folder... \n")
-else:
-	# .json is detected
-	print(f"Detected {JSON_FILE_NAME}.json! \n")
-	MODS_JSON = open(f"{JSON_FILE_NAME}.json")
-	MODS_JSON = MODS_JSON.read()
-	MODS_JSON = json.loads(MODS_JSON)
-
-MODS_FOLDER_PATH = MODS_JSON["mods_folder_path"]
-
+		print(f"Mod info has been written to {JSON_FILE_NAME}.json: Successful \n")
+		path = MODS_JSON["mods_folder_path"]
+		shutil.rmtree(f"{TEMP_DIR}")
+		print("Deleted temp folder... \n")
+	else:
+		# .json is detected
+		print(f"Detected {JSON_FILE_NAME}!")
 
 # B - Check for latest version, and compare against existing versions
 
@@ -115,99 +111,120 @@ params = {'loaders': f"{LOADER}", 'game_versions': f"{TARGET_MINECRAFT_VERSION}"
 # Iterate through all mods in .json
 # Check for latest version on Modrinth
 
-index_number = 0
-mod_order = 1
-for mod in MODS_JSON:
-	if mod == "mods_folder_path":
-		print(f"\nEnd of list in {JSON_FILE_NAME}.json! Mod updates completed!")
-		break
-	else:
-		PROJECT_SLUG = MODS_JSON[mod]["slug"]
-		ABSOLUTE_MOD_PATH = MODS_JSON[mod]["path"]
-		mod_version = MODS_JSON[mod]["version"]
+def check(JSON_FILE):
+	JSON_FILE = f"{os.path.abspath(JSON_FILE)}"
+	MODS_JSON = open(f"{JSON_FILE}")
+	MODS_JSON = MODS_JSON.read()
+	MODS_JSON = json.loads(MODS_JSON)
 
-	PROJECT_GET = requests.get(f"{MODRINTH_URL}/project/{PROJECT_SLUG}/version", headers=header, params=params)
+	MODS_FOLDER_PATH = MODS_JSON["mods_folder_path"]
 
-	HTTP_code_handling(PROJECT_GET.status_code)
-
-	MODRINTH_JSON = json.loads(PROJECT_GET.text)
-	
-	# Find latest mod version with proper Minecraft version and mod loader support
-	for versions in MODRINTH_JSON:
-		if TARGET_MINECRAFT_VERSION not in MODRINTH_JSON[index_number]["game_versions"]:
-			index_number += 1
-			continue
+	index_number = 0
+	mod_order = 1
+	for mod in MODS_JSON:
+		if mod == "mods_folder_path":
+			print(f"\nEnd of list in {JSON_FILE}! Mod updates completed!")
+			break
 		else:
-			if LOADER not in MODRINTH_JSON[index_number]["loaders"]:
+			PROJECT_SLUG = MODS_JSON[mod]["slug"]
+			ABSOLUTE_MOD_PATH = MODS_JSON[mod]["path"]
+			mod_version = MODS_JSON[mod]["version"]
+
+		PROJECT_GET = requests.get(f"{MODRINTH_URL}/project/{PROJECT_SLUG}/version", headers=header, params=params)
+
+		HTTP_code_handling(PROJECT_GET.status_code)
+
+		MODRINTH_JSON = json.loads(PROJECT_GET.text)
+		
+		# Find latest mod version with proper Minecraft version and mod loader support
+		for versions in MODRINTH_JSON:
+			if TARGET_MINECRAFT_VERSION not in MODRINTH_JSON[index_number]["game_versions"]:
 				index_number += 1
 				continue
 			else:
-				latest_version = MODRINTH_JSON[index_number]["version_number"]
-				latest_version = clean_version(latest_version)
-				print(f"\nVersion {latest_version} detected for Minecraft {TARGET_MINECRAFT_VERSION}")
-				break
-
-	# Compare latest version against existing version
-	if True:
-		mod_version = parse(str(mod_version))
-		latest_version = parse(latest_version)
-		# If same version
-		same_version = ""
-		if mod_version < latest_version:
-			status = "newer than"
-		elif mod_version > latest_version:
-			status = "older than"
-		elif mod_version == latest_version:
-			status = "the same version as"
-			same_version = f"No newer version than {mod_version}, nothing to do!\n"
-		print(f"{mod_order}. {mod} -> {mod_version}\n\t"
-			  f"{latest_version} is {status} {mod_version}!"
-			  f"\n\t{same_version}")
-					
-
-		# C - Download newer mod files
-					
-		# If not the same version
-		if same_version == "":
-
-			# Values from JSON about file to download
-			url = MODRINTH_JSON[index_number]["files"][0]["url"]
-			filename = MODRINTH_JSON[index_number]["files"][0]["filename"]
-			sha1_hash = MODRINTH_JSON[index_number]["files"][0]["hashes"]["sha1"]
-
-			# Download from URL and write to file
-			print(f"\tDownloading \"{mod}\" version {latest_version}...")
-			response = requests.get(url, stream=True)
-			with open(f"{filename}", mode="wb") as file:
-				for chunk in response.iter_content(chunk_size=10*1024):
-					file.write(chunk)
-			print(f"\tDownload of \"{filename}\" successful!")
-
-			# Verify checksum
-			with open(f"{filename}", mode="rb") as file:
-				file_hash = hashlib.file_digest(file, "sha1")
-				file_hash = file_hash.hexdigest()
-				print(f"\tTesting SHA1 checksum of \"{filename}\"...")
-
-				# Checksum matches
-				if file_hash == sha1_hash:
-					print("\tChecksum matches! Mod's file integrity confirmed")
-					os.remove(f"{ABSOLUTE_MOD_PATH}")
-					os.replace(f"{filename}", f"{MODS_FOLDER_PATH}/{filename}")
-					print("\tRemoved original file")
-
-					# Writing new values to JSON
-					MODS_JSON[mod]["version"] = f"{latest_version}"
-					MODS_JSON[mod]["path"] = os.path.abspath(f"{MODS_FOLDER_PATH}/{filename}")
-					with open(f"{JSON_FILE_NAME}.json", 'w') as file:
-						json.dump(MODS_JSON, file, indent=4)
-						print(f"\tUpdated \"{JSON_FILE_NAME}.json\"!")
-
-				# Checksum fails
+				if LOADER not in MODRINTH_JSON[index_number]["loaders"]:
+					index_number += 1
+					continue
 				else:
-					print("\tChecksum doesn't match, file may be corrupted!")
-					print("\tKeeping original file...")
-					os.remove(f"{filename}")
-					
-	mod_order += 1
-	index_number = 0
+					latest_version = MODRINTH_JSON[index_number]["version_number"]
+					latest_version = clean_version(latest_version)
+					print(f"\nVersion {latest_version} detected for Minecraft {TARGET_MINECRAFT_VERSION}")
+					break
+
+		# Compare latest version against existing version
+		if True:
+			mod_version = versionParse(str(mod_version))
+			latest_version = versionParse(latest_version)
+			# If same version
+			same_version = ""
+			if mod_version < latest_version:
+				status = "newer than"
+			elif mod_version > latest_version:
+				status = "older than"
+			elif mod_version == latest_version:
+				status = "the same version as"
+				same_version = f"No newer version than {mod_version}, nothing to do!\n"
+			print(f"{mod_order}. {mod} -> {mod_version}\n\t"
+				f"{latest_version} is {status} {mod_version}!"
+				f"\n\t{same_version}")
+						
+
+			# C - Download newer mod files
+						
+			# If not the same version
+			if same_version == "":
+
+				# Values from JSON about file to download
+				url = MODRINTH_JSON[index_number]["files"][0]["url"]
+				filename = MODRINTH_JSON[index_number]["files"][0]["filename"]
+				sha1_hash = MODRINTH_JSON[index_number]["files"][0]["hashes"]["sha1"]
+
+				# Download from URL and write to file
+				print(f"\tDownloading \"{mod}\" version {latest_version}...")
+				response = requests.get(url, stream=True)
+				with open(f"{filename}", mode="wb") as file:
+					for chunk in response.iter_content(chunk_size=10*1024):
+						file.write(chunk)
+				print(f"\tDownload of \"{filename}\" successful!")
+
+				# Verify checksum
+				with open(f"{filename}", mode="rb") as file:
+					file_hash = hashlib.file_digest(file, "sha1")
+					file_hash = file_hash.hexdigest()
+					print(f"\tTesting SHA1 checksum of \"{filename}\"...")
+
+					# Checksum matches
+					if file_hash == sha1_hash:
+						print("\tChecksum matches! Mod's file integrity confirmed")
+						os.remove(f"{ABSOLUTE_MOD_PATH}")
+						os.replace(f"{filename}", f"{MODS_FOLDER_PATH}/{filename}")
+						print("\tRemoved original file")
+
+						# Writing new values to JSON
+						MODS_JSON[mod]["version"] = f"{latest_version}"
+						MODS_JSON[mod]["path"] = os.path.abspath(f"{MODS_FOLDER_PATH}/{filename}")
+						with open(f"{JSON_FILE}", 'w') as file:
+							json.dump(MODS_JSON, file, indent=4)
+							print(f"\tUpdated \"{JSON_FILE}\"!")
+
+					# Checksum fails
+					else:
+						print("\tChecksum doesn't match, file may be corrupted!")
+						print("\tKeeping original file...")
+						os.remove(f"{filename}")
+						
+		mod_order += 1
+		index_number = 0
+
+parse = argparse.ArgumentParser(allow_abbrev=False)
+parse.add_argument("--detect", help="Detect mods from .json file, and create .json (if it doesn't exist)", 
+				   metavar="[file].json", type=str)
+parse.add_argument("--check", help="Check for mod updates and downloads from Modrinth using specified .json",
+				   metavar="[file].json", type=str)
+args = parse.parse_args()
+
+if args.detect:
+	detect(args.detect)
+
+if args.check:
+	check(args.check)
